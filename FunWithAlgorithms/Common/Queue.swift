776abd119c2af47214cd76d6e1ade563e76cc417
Askar <askar.ulubayev@gmail.com>
2019-03-06 21:34:53 +0600

    fixeed queue

--- FunWithAlgorithms/Common/Queue.swift
+++ FunWithAlgorithms/Common/Queue.swift
@@ -9,15 +9,14 @@
 import Foundation
 
 public struct Queue<T> {
-    fileprivate var array = [T?]()
-    fileprivate var head = 0
-    
-    public var isEmpty: Bool {
-        return count == 0
-    }
+    fileprivate var array = [T]()
     
     public var count: Int {
-        return array.count - head
+        return array.count
+    }
+    
+    public var isEmpty: Bool {
+        return array.isEmpty
     }
     
     public mutating func enqueue(_ element: T) {
@@ -25,25 +24,14 @@ public struct Queue<T> {
     }
     
     public mutating func dequeue() -> T? {
-        guard head < array.count, let element = array[head] else { return nil }
-        
-        array[head] = nil
-        head += 1
-        
-        let percentage = Double(head)/Double(array.count)
-        if array.count > 50 && percentage > 0.25 {
-            array.removeFirst(head)
-            head = 0
-        }
-        
-        return element
-    }
-    
-    public var front: T? {
         if isEmpty {
             return nil
         } else {
-            return array[head]
+            return array.removeFirst()
         }
+    }
+    
+    public var front: T? {
+        return array.first
     }
 }
